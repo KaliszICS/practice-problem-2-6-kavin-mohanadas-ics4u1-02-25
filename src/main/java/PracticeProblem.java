16304f4f969e04c635aee3687c822788d32a1eb3
kavin-mohanadas-ics4u1-02-25 <kavin.mohanadas@student.tdsb.on.ca>
2025-04-09 13:54:54 +0000

    updated finished code - kavin

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -5,15 +5,17 @@ public class PracticeProblem {
 	}
 
 	public static int[] bubbleSort(double[] nums) {
+		int n = nums.length;
 		int swaps = 0;
 		int steps = 0;
-		int n = nums.length;
-		for (int i = 0; i < n - 1; i++) {
-			boolean swapped = false;
+		boolean swapped = true;
+
+		for(int i = 0; i < n - 1 && swapped; i++) {
+			swapped = false;
 
 			for (int j = 0; j < n - 1 - i; j++) {
 				steps++;
-				if (nums[j] > nums [j+1]) {
+				if (nums[j] > nums[j+1]) {
 					double temp = nums[j];
 					nums[j] = nums[j+1];
 					nums[j+1] = temp;
@@ -28,131 +30,179 @@ public class PracticeProblem {
 		return new int[] {swaps, steps};
 	}
 
-	public static int[] selectionSort(double[] nums) {
+	public static int[] bubbleSort1(double[] nums) {
+		int n = nums.length;
 		int swaps = 0;
 		int steps = 0;
+		boolean swapped = true;
+		double[] doubleArr = new double[nums.length];
+
+		for (int y = 0; y < n; y++) {
+			doubleArr[y] = nums[y];
+		}
+		for (int i = 0; i < doubleArr.length - 1 && swapped; i++) {
+			swapped = false;
+
+			for (int j = 0; j < doubleArr.length - 1 - i; j++) {
+				steps++;
+				if(doubleArr[j] > doubleArr[j+1]) {
+					double temp = doubleArr[j];
+					doubleArr[j] = doubleArr[j+1];
+					doubleArr[j+1] = temp;
+					swaps += 3;
+					swapped = true;
+				}
+			}
+			if (!swapped) {
+				break;
+			}
+		}
+		return new int[] {swaps, steps};
+	}
+
+	public static int[] selectionSort(double[] nums) {
 		int n = nums.length;
+		int swaps = 0;
+		int steps = 0;
 
 		for (int i = 0; i < n - 1; i++) {
 			int minIndex = i;
-			int updateCount = 0;
-			for (int j = i + 1; j < n; j++) {
+
+			for (int j = i+1; j<n; j++) {
 				steps++;
-				if (nums[j] < nums[minIndex]) {
+				if(nums[minIndex] > nums[j]) {
 					minIndex = j;
-					updateCount++;
+					swaps++;
 				}
 			}
-			if (minIndex != i) {
-				double temp = nums[i];
-				nums[i] = nums[minIndex];
-				nums[minIndex] = temp;
-				swaps += (3 + updateCount);
-			}
-		}
-		if (swaps > 0) {
+			double temp = nums[i];
+			nums[i] = nums[minIndex];
+			nums[minIndex] = temp;
 			swaps += 3;
 		}
-		
+		return new int[] {swaps, steps};
+	}
+
+
+	public static int[] selectionSort1(double [] nums) {
+
+		int n = nums.length;
+		int swaps = 0;
+		int steps = 0;
+		double[] doubleArr = new double[nums.length];
+
+		for (int y = 0; y < n; y++) {
+			doubleArr[y] = nums[y];
+		}
+
+		for (int i = 0; i < doubleArr.length - 1; i++) {
+			int minIndex = i;
+
+			for (int j = i+1; j<n; j++) {
+				steps++;
+				if(doubleArr[minIndex] > doubleArr[j]) {
+					minIndex = j;
+					swaps++;
+				}
+			}
+			double temp = doubleArr[i];
+			doubleArr[i] = doubleArr[minIndex];
+			doubleArr[minIndex] = temp;
+			swaps += 3;
+		}
 		return new int[] {swaps, steps};
 	}
 
 	public static int[] insertionSort(double[] nums) {
+		int n = nums.length;
 		int swaps = 0;
 		int steps = 0;
-		int n = nums.length;
+
 		for (int i = 1; i < n; i++) {
 			double key = nums[i];
 			swaps++;
 			int j = i - 1;
 
-			while (j >= 0) {
-				if(nums[j] > key) {
-					steps++;
+			while (j >= 0 && key < nums[j]) {
+				steps++;
+				if (nums[j] > key) {
 					nums[j+1] = nums[j];
 					swaps++;
 					j--;
+				}
+				else {
+					break;
+				}
+			}
+			nums[j+1] = key;
+			swaps++;
+		}
+		return new int[] {swaps, steps};
+	}
+
+	public static int[] insertionSort1(double[] nums) {
+		int n = nums.length;
+		int swaps = 0;
+		int steps = 0;
+		double[] doubleArr = new double [nums.length];
+
+		for (int y = 0;  y < n; y++) {
+			doubleArr[y] = nums[y];
+		}
+
+		for (int i = 1; i < doubleArr.length; i++) {
+			double key = doubleArr[i];
+			swaps++;
+			int j = i - 1;
+
+			while (j >= 0 && key < doubleArr[j]) {
+				steps++;
+				if(doubleArr[j] > key) {
+					doubleArr[j+1] = doubleArr[j];
+					swaps ++;
+					j--;
 				} else {
 					break;
-		}
-		
+				}
 			}
-		
-		nums[j+1] = key;
-		swaps++;
+			doubleArr[j+1] = key;
+			swaps++;
+		}
+		return new int[] {swaps, steps};
 	}
-	return new int[] {swaps, steps};
-
-
-}
 
 	public static String leastSwaps(double[] nums) {
-		double[] arrBubble = new double[nums.length];
-		double[] arrSelection = new double[nums.length];
-		double[] arrInsertion = new double[nums.length];
-		for (int i = 0; i < nums.length; i++) {
-			arrBubble[i] = nums[i];
-			arrSelection[i] = nums[i];
-			arrInsertion[i] = nums[i];
-		}
-		int[] bubbleMetrics = bubbleSort(arrBubble);
-		int[] selectionMetrics = selectionSort(arrSelection);
-		int[] insertionMetrics = insertionSort(arrInsertion);
+		if(insertionSort1(nums)[0] < bubbleSort1(nums)[0]) {
+			if(selectionSort1(nums)[0] < insertionSort(nums)[0]) {
+				return "Selection";
 
-		int bubbleSwaps = bubbleMetrics[0];
-		int selectionSwaps = selectionMetrics[0];
-		int insertionSwaps = insertionMetrics[0];
-
-		int min = bubbleSwaps;
-		if (selectionSwaps < min) {
-			min = selectionSwaps;
 		}
-		if (insertionSwaps < min) {
-			min = insertionSwaps;
-		}
-		if (bubbleSwaps == min) {
-			return "Bubble";
-		}
-		if (insertionSwaps == min) {
+		 else {
 			return "Insertion";
 		}
-		return "Selection";
+	}
+		else if (selectionSort(nums)[0] < bubbleSort(nums)[0]) {
+			return "Selection";
+		} else {
+			return "Bubble";
+		}
 	}
 
 	public static String leastIterations(double[] nums) {
-		double[] arrBubble = new double[nums.length];
-		double[] arrSelection = new double[nums.length];
-		double[] arrInsertion = new double[nums.length];
-		for (int i = 0; i < nums.length; i++) {
-			arrBubble[i] = nums[i];
-			arrSelection[i] = nums[i];
-			arrInsertion[i] = nums[i];
-		}
-		int[] bubbleMetrics = bubbleSort(arrBubble);
-		int[] selectionMetrics = selectionSort(arrSelection);
-		int[] insertionMetrics = insertionSort(arrInsertion);
+		if(insertionSort1(nums)[1] < bubbleSort1(nums)[1]) {
+			if(selectionSort1(nums)[1] < insertionSort(nums)[1]) {
+				return "Selection";
 
-		int bubbleSteps = bubbleMetrics[1];
-		int selectionSteps = selectionMetrics[1];
-		int insertionSteps = insertionMetrics[1];
-
-		int min = bubbleSteps;
-		if (selectionSteps < min) {
-			min = selectionSteps;
 		}
-		if (insertionSteps < min) {
-			min = insertionSteps;
-		}
-		if (bubbleSteps == min) {
-			return "Bubble";
-		}
-		if (insertionSteps == min) {
+		 else {
 			return "Insertion";
 		}
-		return "Selection";
+	}
+		else if (selectionSort(nums)[1] < bubbleSort(nums)[1]) {
+			return "Selection";
+		} else {
+			return "Bubble";
+		}
+		
 	}
 }
-
-	
-
-	
